94fae26dab73bc03bfae301bce5b3f5640587c17
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-30 04:34:43 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
                      action: 'new') if !params[:name] || params[:name].empty?
   session[:name] = params[:name]
   redirect_to controller: 'application', action: 'hello'
-end
+  end
 
 
   def new

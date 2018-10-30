a44f6d78dada3f4bea39a2b2cc34cd139c01192d
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-30 04:16:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
-class SessionsController < ActionController::Base
+class SessionsController < ApplicationController
 
   def create
     binding.pry
     session[:username] = params[:username]
     redirect_to '/sessions/new'
+  end
+
+  def new
+  end
+
+  def destroy
   end
 end

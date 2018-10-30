4a0e5ed126874bd64e5dcbe00928da487b421687
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-30 04:34:38 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,23 @@
 class SessionsController < ApplicationController
 
+  # def create
+  #   if params[:name] == nil || params[:name].strip == ""
+  #     redirect_to '/login'
+  #   elsif params[:name]
+  #     session[:name] = params[:name]
+  #   end
+  #   if session[:name] = params[:name]
+  #     redirect_to '/'
+  #   end
+  # end
+
   def create
-    # binding.pry
-    if params[:name] == nil || params[:name].strip == ""
-      redirect_to '/login'
-    elsif params[:name]
-      session[:name] = params[:name]
-    end
-    if session[:name] = params[:name]
-      redirect_to '/'
-    end
-  end
+  return redirect_to(controller: 'sessions',
+                     action: 'new') if !params[:name] || params[:name].empty?
+  session[:name] = params[:name]
+  redirect_to controller: 'application', action: 'hello'
+end
+
 
   def new
   end
